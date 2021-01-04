a3d5d5174cefca8cbb9569ba92f0ee256361a3e8
Shaun Mulligan <switchflip.shaun@gmail.com>
2021-01-04 17:19:27 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://twitter.com/shaunmulligan9">@shaunmulligan9</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+//         <span>
+//           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+//           <a href="https://thriveweb.com.au/">Thrive</a>.
+//         </span>
       </div>
     </footer>
   </div>
